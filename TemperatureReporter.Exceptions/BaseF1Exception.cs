34ea7bb44b2750cd444c4185213c9ea5d2ed738d
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2016-01-25 19:04:06 +0530

    adding the base exception class

---
+++ TemperatureReporter.Exceptions/BaseF1Exception.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureReporter.Exceptions
+{
+    /// <summary>
+    /// This is the base exception which will be the base class
+    /// for all our custom exceptions. Any common logic would be pulled
+    /// into this layer
+    /// </summary>
+    class BaseF1Exception: Exception
+    {
+    }
+}
