9328f77b7af5953e3e595cb5570b2fe3ebbdc909
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2016-01-27 23:16:53 +0530

    fixing morris chart

--- TemperatureReporter.Web/Controllers/HomeController.cs
+++ TemperatureReporter.Web/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Metadata.Edm;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TemperatureReporter.Contracts.Input;
+using TemperatureReporter.Implementation.Input;
 
 namespace TemperatureReporter.Web.Controllers
 {
@@ -10,7 +14,12 @@ namespace TemperatureReporter.Web.Controllers
     {
         //
         // GET: /Home/
+        private IInputTemperatureFileReader fileReader;
 
+        public HomeController()
+        {
+            this.fileReader = new InputTemperatureFileReader();
+        }
         public ActionResult Index()
         {
             return View();
@@ -32,6 +41,30 @@ namespace TemperatureReporter.Web.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Input/"), fileName);
+                    file.SaveAs(path);
+                    var logs = fileReader.ReadTyreTemperatures(path);
+                }
+            }
+
+            return RedirectToAction("Results");
+        }
+
+        public ActionResult Results()
+        {
+            return View();
+        }
+
         //
         // POST: /Home/Create
 
@@ -48,6 +81,20 @@ namespace TemperatureReporter.Web.Controllers
             {
                 return View();
             }
+        }
+
+
+        public ActionResult GetData()
+        {
+            var data = new[] { new Entry() { value = 20, xaxis= 2008 }, new Entry() { value = 10, xaxis= 2009 } };
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public class Entry
+        {
+            public int xaxis { get; set; }
+            public int value { get; set; }
         }
 
         //
